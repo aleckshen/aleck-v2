4229918b8de9fbfae5b596718b4e845bc1cffdc7
aleckshen <aleckshn@gmail.com>
2025-10-17 01:09:25 +1300

    changed info and font size for hero

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
 
       <div className="flex justify-between flex-col h-screen">
         <NavBar />
-        <div className="flex justify-end mt-[175px] mb-[175px] mr-[50px]">
-          <div className="mr-[250px]">
-            Hello, I'm Aleck! <br />
-            I'm a second year computer science major studying <br />
-            at the University Of Auckland
+        <div className="flex justify-end mt-[175px] mb-[175px] mr-[75px]">
+          <div className="mr-[125px]">
+            <span className="text-[25px]">Hello, my name is Aleck!</span> <br />
+            I'm a second year computer science major studying at the University Of<br />
+            Auckland, passionate about coding and problem solving.
           </div>
           <div>
             *CONNECT WITH ME
