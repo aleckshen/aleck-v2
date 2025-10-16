c39fb9f43ee2fc9a33dafe4f41eeee83161b0fa9
aleckshen <aleckshn@gmail.com>
2025-10-16 21:24:52 +1300

    added description section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,22 @@ import NavBar from '@/components/NavBar'
 
 export default function Home() {
   return (
-    <NavBar />
+    <div>
+
+      <div className="flex flex-col h-screen">
+        <NavBar />
+        <div className="flex justify-end mt-[175px] mb-[175px] mr-[50px]">
+          <div className="mr-[250px]">
+            Hello, I'm Aleck! <br />
+            I'm a second year computer science major studying <br />
+            at the University Of Auckland
+          </div>
+          <div>
+            *CONNECT WITH ME
+          </div>
+        </div>
+      </div>
+      
+    </div>
   );
 }
