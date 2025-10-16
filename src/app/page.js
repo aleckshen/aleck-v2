598fc3f7c88a38c45435e0ed5c725dc12a446d8a
aleckshen <aleckshn@gmail.com>
2025-10-16 22:06:09 +1300

    added title name to bottom of homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import NavBar from '@/components/NavBar'
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-hidden">
 
-      <div className="flex flex-col h-screen">
+      <div className="flex justify-between flex-col h-screen">
         <NavBar />
         <div className="flex justify-end mt-[175px] mb-[175px] mr-[50px]">
           <div className="mr-[250px]">
@@ -15,6 +15,10 @@ export default function Home() {
           <div>
             *CONNECT WITH ME
           </div>
+        </div>
+        <div>
+          <div className="ml-[15px] text-[150px] leading-none font-medium">ALECK</div>
+          <div className="text-[150px] ml-[275px] leading-none font-medium">SHEN</div>
         </div>
       </div>
       
